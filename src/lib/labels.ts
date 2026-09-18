fd2bbbbae0f02cb916020de30be1c71580b80682
eottabom <snoopy12oyk@gmail.com>
2026-09-18 21:12:54 +0900

    chore: drop label constants nothing references
    - five constants in lib/labels.ts had no reader anywhere in src
    - SIDECAR_INJECT_LABEL also duplicated SIDECAR_INJECT_ANNOTATION verbatim

--- src/lib/labels.ts
+++ src/lib/labels.ts
@@ -3,19 +3,14 @@
 export const DATAPLANE_MODE = 'istio.io/dataplane-mode';
 export const SIDECAR_INJECTION = 'istio-injection';
 export const REVISION = 'istio.io/rev';
+/** Set as either a label or an annotation; `podMeshState` checks both. */
 export const SIDECAR_INJECT_ANNOTATION = 'sidecar.istio.io/inject';
-export const SIDECAR_INJECT_LABEL = 'sidecar.istio.io/inject';
-export const SIDECAR_STATUS = 'sidecar.istio.io/status';
 
 export const USE_WAYPOINT = 'istio.io/use-waypoint';
 export const USE_WAYPOINT_NAMESPACE = 'istio.io/use-waypoint-namespace';
 export const WAYPOINT_FOR = 'istio.io/waypoint-for';
 
-export const GATEWAY_NAME = 'gateway.networking.k8s.io/gateway-name';
-export const ISTIO_GATEWAY_NAME = 'istio.io/gateway-name';
-
 export const WAYPOINT_GATEWAY_CLASS = 'istio-waypoint';
-export const ISTIO_GATEWAY_CLASSES = ['istio', 'istio-waypoint', 'istio-remote'];
 
 export const PART_OF = 'app.kubernetes.io/part-of';
 export const APP_NAME = 'app.kubernetes.io/name';
